a61e84d79d67559f408c2bd4dfeaa1ae4c04ce4e
diggy-mo <rereon@keio.jp>
2016-10-11 13:44:57 +0900

    update

--- grid_map.c
+++ grid_map.c
@@ -17,7 +17,9 @@ int read_data(){
     x0 = 0.001 * x0;
     y0 = 0.001 * y0;
     z0 = 0.001 * z0;
-    if(z0 < h_limit && -h_limit < z0 && x0 * x0 + y0 * y0 < dist_limit * dist_limit){
+    if(z0 < h_limit && -h_limit < z0 && x0*x0 + y0*y0 < dist_limit * dist_limit && 0 <= x0){
+      printf("%f\t%f\n",x0,y0);
+      
       vertex[i][0] = x0;
       vertex[i][1] = y0;
       vertex[i][2] = z0 + lrf_heigt;
@@ -26,14 +28,18 @@ int read_data(){
   }
   fclose(fp);
   // printf("%d\n",i);
-  return i-1;
+  //feofが一行多く読み込んでしまう．最後の行でx0が負の時，Point_Numに影響がでる
+  if(x0 > 0)
+    return i-1;
+  else
+    return i;
 }
  
 
 void create_grid(int Point_Num){
   int i,j;
   float grid_interval = 0.5;
-  float tmp_x,tmp_y;
+  int tmp_x,tmp_y;
   int dem_ID;
   //----------------
   //            x,i=15
@@ -50,10 +56,13 @@ void create_grid(int Point_Num){
       dem[i*32+j][2] = 0.0 ;
     }
   }
+  //printf("%f,%f\n",dem[0][1],dem[511][1]);
   for(i=0;i<Point_Num;i++){
-    tmp_x = (int) roundf(vertex[i][0]/grid_interval);
-    tmp_y = (int) roundf(vertex[i][1]/grid_interval);
-    dem_ID =  15 + tmp_y + tmp_x*32;
+    tmp_x =  vertex[i][0]/grid_interval;
+    tmp_y =  vertex[i][1]/grid_interval - 1;
+    printf("%d,%d\t",tmp_x,tmp_y);
+    dem_ID =  16 + tmp_y + tmp_x*32;
+    printf("%d\n",dem_ID);
 
     if(dem[dem_ID][2]<vertex[i][2])
       dem[dem_ID][2] = vertex[i][2];
@@ -65,5 +74,5 @@ void create_grid(int Point_Num){
   //   i+1   i   i-1
   //        i-32
   //----------------------
-
+  
 }
